9e2cd41318c4745acdb8075365a12b7853269b66
indrajit1221 <indrajit@influxiqtech.com>
2024-08-28 15:57:44 +0530

    update

---
+++ pages/consumer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div><img src='https://all-frontend-assets.s3.amazonaws.com/club-rx-pro/consumer_landing_page/consumer_landing_page.jpg' style={{display: 'block', width: '100%'}}></img></div>
+
+  )
+}
+
+export default index
